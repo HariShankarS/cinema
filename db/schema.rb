e3a619c3b35b8fe1cdc66579f80e16721066878a
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-01-02 18:11:26 +0530

    Changes in Sign-in-up Pages and added title in showpage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227085208) do
+ActiveRecord::Schema.define(version: 20161230083929) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20161227085208) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
